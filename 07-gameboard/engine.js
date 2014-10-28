b5f3824d46790be4b48751c90b19cbc055f4f03f
Chacarron <chacapro18@gmail.com>
2014-10-28 11:31:17 +0100

    Terminado la Parte 07

--- 07-gameboard/engine.js
+++ 07-gameboard/engine.js
@@ -208,19 +208,18 @@ var GameBoard = function() {
     this.iterate = function(funcName) {
 	// Convertimos en un array args (1..)
 	var args = Array.prototype.slice.call(arguments,1);
+	// Cambiamos iterate para realizar la iteración con underscore
+	_.each(this.objects, function(obj){obj[funcName].apply(obj,args)});
 
-	for(var i=0, len=this.objects.length; i<len;i++) {
-	    var obj = this.objects[i];
-	    obj[funcName].apply(obj,args)
-	}
     };
 
     // Devuelve el primer objeto de objects para el que func es true
     this.detect = function(func) {
-	for(var i = 0,val=null, len=this.objects.length; i < len; i++) {
-	    if(func.call(this.objects[i])) return this.objects[i];
-	}
-	return false;
+	var val = _.filter(this.objects,function(obj){return func.call(obj)});
+    	if(val)
+		return val[0]; 
+	else	
+		return false;
     };
 
     // Cuando Game.loop() llame a step(), hay que llamar al método
